76f0faf15050805e102d2f02a4081dfba319296d
YungChun Tu <ken888686@gmail.com>
2025-05-29 13:11:51 +0900

    Add UserController

--- src/UserManagementService.Infrastructure/Services/UserService.cs
+++ src/UserManagementService.Infrastructure/Services/UserService.cs
@@ -7,9 +7,9 @@ namespace UserManagementService.Infrastructure.Services;
 
 public class UserService(IUserRepository repository, IMapper mapper) : IUserService
 {
-    public async Task<UserDto> GetUserAsync(Guid id)
+    public async Task<UserDto?> GetUserByAsync(Guid id)
     {
         var user = await repository.GetByIdAsync(id);
-        return user is null ? new UserDto() : mapper.Map<UserDto>(user);
+        return user is null ? null : mapper.Map<UserDto>(user);
     }
 }
